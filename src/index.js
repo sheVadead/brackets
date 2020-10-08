08ee7235deeab278f864b5587fabec459d5bd644
Dmitry Shevchenok <dm.shevchenok@gmail.com>
2020-10-08 11:29:46 +0300

    feat: added solution for brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check (str, bracketsConfig) {
+    const res = [];
+    const brackets = Object.fromEntries(bracketsConfig);
+    debugger
+    for (const el of str.split``) {
+        if (res[0] === el) {
+            res.shift();
+        } else if (brackets[el]) {
+            res.unshift(brackets[el]);
+        } else {
+            return false;
+        }
+    }
+
+    return res.length === 0;
 }
